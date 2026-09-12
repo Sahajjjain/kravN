b5f6954e6a24e5e833b1567a3c51788defc57882
Sahaj Jain <jainsahaj167@gmail.com>
2026-09-12 22:09:36 +0530

    Changes revived as there were errors

--- server.js
+++ server.js
@@ -62,7 +62,6 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax'
   }
 }));
